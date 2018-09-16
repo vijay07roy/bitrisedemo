1439b0982ba8d43cce611e71cb28fea71e9d3557
vijay07roy <34807718+vijay07roy@users.noreply.github.com>
2018-09-16 20:18:38 +0530

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,46 +1,7 @@
 #!/bin/bash
 
-# Copyright (c) 2016-2018 Martin Donath <martin.donath@squidfunk.com>
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-
 # Exit, if one command fails
 set -e
-
-# # Create directory for overrides, so we don't clutter up the base theme with
-# # our custom adjustments for our own hosted documentation
-# mkdir -p overrides
-# cat > overrides/main.html <<-EOM
-#   {% extends "base.html" %}
-#   {% block scripts %}
-#     {{ super() }}
-#     <script>
-#       (function(i,s,o,g,r,a,m){
-#         i["GinsengAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||
-#         []).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-#         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
-#         m.parentNode.insertBefore(a,m)
-#       })(window, document,
-#         "script", "https://ginseng.ai/analytics.js", "gx");
-#     </script>
-#   {% endblock %}
-# EOM
 
 # Deploy documentation to GitHub pages
 if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
